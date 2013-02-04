fb2ef6c0b6ff0108ab25641ae78db0b174044eab
Serjeo722 <7221798@gmail.com>
2013-02-04 16:45:54 +0400

    TreeMap

--- BinarySearch/src/Util/Main.java
+++ BinarySearch/src/Util/Main.java
@@ -18,11 +18,14 @@ public class Main {
 		}
 		
 		Iterator2d<ArrayList<String>> i = table.iterator2d();
+		
 		while (i.hasNext()){
+			
 			ArrayList<String> strings=i.next();
+			System.out.println("key="+i.getKey()+" x=" + i.getX() + " y=" + i.getY());
 			
 			for(String s:strings)
-				System.out.println("key="+i.getKey()+" x=" + i.getX() + " y=" + i.getY() + " value=" + s);
+				System.out.println(" value=" + s);
 			
 		}
 		
--- BinarySearch/src/Util/SparseIntIntArray.java
+++ BinarySearch/src/Util/SparseIntIntArray.java
@@ -1,78 +1,81 @@
 package Util;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class SparseIntIntArray<V> {
 
 	private static final int BITS_MASK = 32;
 	
-	private ArrayList<Long> keys = new ArrayList <Long>();
-	private ArrayList<ArrayList<V>> values = new ArrayList <ArrayList<V>>();
+	private SortedMap<Long,ArrayList<V>> map=new TreeMap<Long,ArrayList<V>>();
 
 	public void put(int x,int y, V value){
 		long key = ((long)(x) << BITS_MASK) + y;
 
-		int idx = Collections.binarySearch(keys, key);
-		if (idx >= 0) {
-			keys.add(idx, key);
-			values.get(idx).add(value);
-		} else {
-			int i=-idx - 1;
-			keys.add(i, key);
-			values.add(i,new ArrayList<V>());
-			values.get(i).add(value);
+		if(map.containsKey(key)){
+			ArrayList<V> array = map.get(key);
+			array.add(value);
+		}else{
+			ArrayList<V> array = new ArrayList<V>();
+			array.add(value);
+			map.put(key, array);
 		}
 	}
+
+	private Iterator2d<ArrayList<V>> iter = new SparseIntIntArrayIterator(map);
 	
 	public Iterator2d<ArrayList<V>> iterator2d(){
 		return iter;
 	}
 	
-	private Iterator2d<ArrayList<V>> iter = new SparseIntIntArrayIterator(keys, values);
-	
 	private class SparseIntIntArrayIterator implements Iterator2d<ArrayList<V>>{
-		private ArrayList<Long> keys;
-		private ArrayList<ArrayList<V>> values;
-		private int pos = 0;
+		private Iterator<Entry<Long,ArrayList<V>>> iterator;
+		private Entry<Long,ArrayList<V>> entry;
+		private SortedMap<Long,ArrayList<V>> map;
+		private boolean started=false;
 		
-		public SparseIntIntArrayIterator(ArrayList<Long> keys, ArrayList<ArrayList<V>> values){
-			this.keys = keys;
-			this.values = values;
+		public SparseIntIntArrayIterator(SortedMap<Long,ArrayList<V>> map){
+			this.map = map;
+			iterator = map.entrySet().iterator();
 		}
+		
 		
 		@Override
 		public boolean hasNext() {
-			if(pos>values.size()-1) return false;
-			return true;
+			if(!started){
+				iterator = map.entrySet().iterator();
+				started=true;
+			}
+			return iterator.hasNext();
 		}
 
 		@Override
 		public ArrayList<V> next() {
-			pos++;
-			return values.get(pos - 1);
+			entry = iterator.next();
+			return entry.getValue();
 		}
 
 		@Override
 		public void remove() {
-			pos--;
-			keys.remove(pos);
-			values.remove(pos);
+			iterator.remove();
 		}
 
 		@Override
 		public int getX() {
-			return (int) (keys.get(pos-1)>>BITS_MASK);
+			return (int) (entry.getKey()>>BITS_MASK);
 		}
 
 		@Override
 		public int getY() {
-			return (int)(keys.get(pos-1)-((long)getX()<<BITS_MASK));
+			return (int)(entry.getKey()-((long)getX()<<BITS_MASK));
 		}
 
 		@Override
 		public long getKey() {
-			return keys.get(pos-1);
+			return entry.getKey();
 		}
 	}
 }
